a1fb4289a1f550d8ee3689c312d8afa7800ee5ec
AlienX-stack <84347170+AlienX-stack@users.noreply.github.com>
2022-07-31 20:18:52 +0530

    Create palindromeString.java

---
+++ palindromeString.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class HelloWorld {
+    static boolean checkPalindrome(char ch[],int l,int r){
+        if(l>=r/2)
+        {
+            return true;
+        }
+        // System.out.print(ch[l]);
+        if(ch[l]!=ch[r-l-1])
+        {
+            return false;
+        }
+        return checkPalindrome(ch,l+1,r);
+    }
+    public static void main(String[] args) {
+        String s = "M";
+        char ch[] = s.toCharArray();
+        System.out.print(checkPalindrome(ch,0,ch.length));
+    }
+}
